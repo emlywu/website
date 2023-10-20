122ca49da5e94a6526471f2094fbb38d794cb6c8
Emily Wu <72621829+ew432@users.noreply.github.com>
2023-10-19 22:45:04 -0400

    fix date

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -22,7 +22,7 @@ export default function Experience() {
             </li>
             <li class="mb-10 ml-4">
               <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-              <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">July 2023 - Present</time>
+              <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">June 2022 - August 2022</time>
               <h3 class="text-lg font-semibold">Software Engineer Intern @ <a target="_blank" href="https://www.qualtrics.com/" className="company">Qualtrics</a></h3>
               <div className="badge badge-primary badge-outline">AngularJS</div>
               <div className="badge badge-primary badge-outline">Java</div>
